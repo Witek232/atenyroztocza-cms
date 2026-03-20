c68c0893f0e26cc52ad62701ec78c6139db9cb1b
Witek232 <witek232@yandex.com>
2026-03-20 19:10:13 +0100

    Create index.ts

---
+++ src/schemaTypes/index.ts
@@ -0,0 +1,17 @@
+import ustawieniaStrony from './ustawieniaStrony'
+import nawigacja from './nawigacja'
+import wydarzenie from './wydarzenie'
+import galeria from './galeria'
+import materialAudiowizualny from './multimedia'
+import artykul from './artykul'
+import kategoria from './kategoria'
+
+export const schemaTypes = [
+  ustawieniaStrony,
+  nawigacja,
+  wydarzenie,
+  galeria,
+  materialAudiowizualny,
+  artykul,
+  kategoria,
+]
