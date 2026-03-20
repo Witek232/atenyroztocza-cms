0c2a5381c44ce1c2c26b56348bd4cec17fb569a8
Witek232 <witek232@yandex.com>
2026-03-20 19:29:11 +0100

    Update index.ts

--- src/schemaTypes/index.ts
+++ src/schemaTypes/index.ts
@@ -1,12 +1,30 @@
-import ustawieniaStrony from './ustawieniaStrony'
-import nawigacja from './nawigacja'
-import wydarzenie from './wydarzenie'
-import galeria from './galeria'
-import materialAudiowizualny from './multimedia'
-import artykul from './artykul'
-import kategoria from './kategoria'
+// Documents
+import { ustawieniaStrony } from './documents/ustawieniaStrony'
+import { nawigacja } from './documents/nawigacja'
+import { wydarzenie } from './documents/wydarzenie'
+import { galeria } from './documents/galeria'
+import { materialAudiowizualny } from './documents/multimedia'
+import { artykul } from './documents/artykul'
+import { kategoria } from './documents/kategoria'
+
+// Objects (reużywalne)
+import { blockContent } from './objects/blockContent'
+import { seo } from './objects/seo'
+import { linkZewnetrzny } from './objects/link'
+import { tlumaczenieTekstowe } from './objects/tlumaczenia'
+import { zdjecieZPodpisem } from './objects/zdjecie'
+import { youtubeEmbed } from './objects/youtube'
 
 export const schemaTypes = [
+  // Objects (muszą być pierwsze!)
+  blockContent,
+  seo,
+  linkZewnetrzny,
+  tlumaczenieTekstowe,
+  zdjecieZPodpisem,
+  youtubeEmbed,
+
+  // Documents
   ustawieniaStrony,
   nawigacja,
   wydarzenie,
