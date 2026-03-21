067ee8793ade9051df393a22db2b92118e9a8c68
Witek232 <witek232@yandex.com>
2026-03-21 18:32:36 +0100

    Update sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,7 +2,7 @@ import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.SANITY_STUDIO_PROJECT_ID || 'w2r65fb5',
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID || '8bab26y8',
     dataset: process.env.SANITY_STUDIO_DATASET || 'production',
   },
   studioHost: 'ateny-roztocza',
